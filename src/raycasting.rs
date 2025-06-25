5e18dfdbd9d77e379b644baf287b0e3c3da28623
will-maclean <macleanwill1@gmail.com>
2025-06-25 22:11:42 +1000

    more tests

--- src/raycasting.rs
+++ src/raycasting.rs
@@ -1,6 +1,4 @@
-use std::default;
-
-use cgmath::{Point3, Vector3};
+use cgmath::{num_traits::float::TotalOrder, Point3, Vector3};
 
 use crate::camera::Camera;
 
@@ -69,31 +67,105 @@ pub fn get_colliding_face(
     // point.
     // FIXME: this is a gross disgusting method that will
     // cause weird behaviour - might work well enough for now though
+    // TODO: need to check that collision_pos is contained in the block
     let block_loc = block_loc.cast::<f32>().unwrap();
-    let x_pos_dist = collision_pos.x - block_loc.x;
-    let x_neg_dist = block_loc.x + 1.0 - collision_pos.x;
-    let y_pos_dist = collision_pos.y - block_loc.y;
-    let y_neg_dist = block_loc.y + 1.0 - collision_pos.y;
-    let z_pos_dist = collision_pos.z - block_loc.z;
-    let z_neg_dist = block_loc.z + 1.0 - collision_pos.z;
+    let x_pos_dist = block_loc.x + 1.0 - collision_pos.x;
+    let x_neg_dist = collision_pos.x - block_loc.x;
+    let y_pos_dist = block_loc.y + 1.0 - collision_pos.y;
+    let y_neg_dist = collision_pos.y - block_loc.y;
+    let z_pos_dist = block_loc.z + 1.0 - collision_pos.z;
+    let z_neg_dist = collision_pos.z - block_loc.z;
 
     let dists = vec![
         x_pos_dist, x_neg_dist, y_pos_dist, y_neg_dist, z_pos_dist, z_neg_dist,
     ];
 
-    match dists
-        .iter()
+    if let Some(amin) = argmin(&dists) {
+        match amin {
+            0 => Some(BlockFace::XPos),
+            1 => Some(BlockFace::XNeg),
+            2 => Some(BlockFace::YPos),
+            3 => Some(BlockFace::YNeg),
+            4 => Some(BlockFace::ZPos),
+            5 => Some(BlockFace::ZNeg),
+            _ => None,
+        }
+    } else {
+        None
+    }
+}
+
+pub fn argmin<T: TotalOrder>(v: &[T]) -> Option<usize> {
+    v.iter()
         .enumerate()
         .min_by(|(_, a), (_, b)| a.total_cmp(b))
         .map(|(index, _)| index)
-        .unwrap()
-    {
-        0 => Some(BlockFace::XPos),
-        1 => Some(BlockFace::XNeg),
-        2 => Some(BlockFace::YPos),
-        3 => Some(BlockFace::YNeg),
-        4 => Some(BlockFace::ZPos),
-        5 => Some(BlockFace::ZNeg),
-        _ => None,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_argmin() {
+        assert_eq!(argmin(&vec![0.0, 1.0, 2.0]), Some(0));
+        assert_eq!(argmin(&vec![0.0, -1.0, 2.0]), Some(1));
+    }
+
+    #[test]
+    fn test_get_colliding_face() {
+        assert_eq!(
+            get_colliding_face(
+                Point3::new(0.0, 0.0, 0.0),
+                Point3::new(1.01, 0.5, 0.5),
+                Point3::new(1, 0, 0)
+            ),
+            Some(BlockFace::XNeg),
+        );
+
+        assert_eq!(
+            get_colliding_face(
+                Point3::new(0.0, 0.0, 0.0),
+                Point3::new(1.99, 0.5, 0.5),
+                Point3::new(1, 0, 0)
+            ),
+            Some(BlockFace::XPos),
+        );
+
+        assert_eq!(
+            get_colliding_face(
+                Point3::new(0.0, 0.0, 0.0),
+                Point3::new(1.5, 0.1, 0.5),
+                Point3::new(1, 0, 0)
+            ),
+            Some(BlockFace::YNeg),
+        );
+
+        assert_eq!(
+            get_colliding_face(
+                Point3::new(0.0, 0.0, 0.0),
+                Point3::new(1.5, 0.9, 0.5),
+                Point3::new(1, 0, 0)
+            ),
+            Some(BlockFace::YPos),
+        );
+
+        assert_eq!(
+            get_colliding_face(
+                Point3::new(0.0, 0.0, 0.0),
+                Point3::new(1.5, 0.5, 0.1),
+                Point3::new(1, 0, 0)
+            ),
+            Some(BlockFace::ZNeg),
+        );
+
+        assert_eq!(
+            get_colliding_face(
+                Point3::new(0.0, 0.0, 0.0),
+                Point3::new(1.5, 0.5, 0.9),
+                Point3::new(1, 0, 0)
+            ),
+            Some(BlockFace::ZPos),
+        );
     }
 }
